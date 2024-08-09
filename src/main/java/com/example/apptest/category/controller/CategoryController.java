b6145335ca3c3ada74cd5147a398d37f092bb840
Joojinsung <dev.jinsung1017@gmail.com>
2024-08-09 18:32:29 +0900

    category

--- src/main/java/com/example/apptest/category/controller/CategoryController.java
+++ src/main/java/com/example/apptest/category/controller/CategoryController.java
@@ -2,11 +2,11 @@ package com.example.apptest.category.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/category")
+@RestController
+@RequestMapping("/api")
 public class CategoryController {
-
-
 
     @GetMapping("/get")
     public String getCategory() {
